d6951f76086f06a6db848dd73964a457aec41d90
Mario Carande Scarafia <marioc@nimble.la>
2023-02-12 15:44:16 -0300

    UPDATE: change class style

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -11,11 +11,11 @@ const Pokemon = ({ name }) => {
         .catch((err) => console.log(err))
     }
   }, [name]);
-  console.log(pokemon)
+
   return (
     <div className="pokemon-information">
       <span>Name: {pokemon ? pokemon.name : 'Loading'}</span>
-      <div style={{ display: 'flex', backgroundColor: '#A29F9F', width: '200px', height: '200px', alignItems: 'center', justifyContent: 'center' }}>
+      <div className='image'>
         {pokemon ?
           <img style={{ height: '200px' }} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="pokemon" /> :
           <img style={{ height: '200px' }} src={`https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png`} alt="pokebola" />}
