a9a3ff7101d6b180596a31ed6ebc36afa2e9fc55
Mario Carande Scarafia <marioc@nimble.la>
2023-02-12 16:58:01 -0300

    UPDATE: styles and show habilities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import './App.css';
 import Pokemon from './Pokemon';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [search, setSearch] = useState('');
@@ -22,14 +25,26 @@ function App() {
 
   return (
     <div className="App">
-      <div className="pokedex">
-        <div className="search-bar">
-          <input id="myInput" type="text" value={search} onChange={handleChange} onKeyPress={handleKeyPress} />
-          <button id="myBtn" onClick={handleClick}>Search</button>
+      <Container>
+        <div className="pokedex">
+            <Row className="overflow-hidden w-100">
+              <Col xs={12} md={5} >
+                  <div className="search-bar">
+                    <input id="myInput" type="text" value={search} onChange={handleChange} onKeyPress={handleKeyPress} />
+                    <button id="myBtn" onClick={handleClick}>Search</button>
+                </div>
+              </Col>
+              <Col xs={12} md={2}>
+                <div className="divisor-container">
+                  <div className="divisor-line" />
+                </div>
+              </Col>
+              <Col xs={12} md={5}>
+                <Pokemon name={name} />
+              </Col>
+            </Row>
         </div>
-        <span className='divisor-line'/>
-        <Pokemon name={name} />
-      </div>
+      </Container>
     </div>
   );
 }
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -12,14 +12,24 @@ const Pokemon = ({ name }) => {
     }
   }, [name]);
 
+  // if (!pokemon) return null;
+
   return (
     <div className="pokemon-information">
-      <span>Name: {pokemon ? pokemon.name : 'Loading'}</span>
       <div className='image'>
         {pokemon ?
           <img style={{ height: '200px' }} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="pokemon" /> :
           <img style={{ height: '200px' }} src={`https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png`} alt="pokebola" />}
       </div>
+      <span>Name: {pokemon?.name}</span>
+      <span>Altura: {pokemon?.height / 10} m</span>
+      <span>Peso: {pokemon?.weight / 10} kg</span>
+      <span>Abilities:
+        <ul>
+          {pokemon?.abilities.map(ability => <li>{ability.ability.name}</li>)}
+      </ul>
+      </span>
+
     </div>
   )
 }
