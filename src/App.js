f212164ed08ddcbc069545476cd8bd99d567c03e
Mario Carande Scarafia <marioc@nimble.la>
2023-02-15 15:56:29 -0300

    UPDATE: styles and add type and pokemon number

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Pokemon from './Pokemon';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import pokemonLogo from './images/pokemon_logo.png';
+import pokemonLogo from './assets/images/pokemon_logo.png';
 import API from './api';
 
 function App() {
@@ -42,58 +42,77 @@ function App() {
       <Container>
         <div className="pokedex">
           <Row className="rowstyle">
-              <Col xs={12} md={5}>
-                <Row>
-                  <div className="search-bar">
-                  <input
-                    id="myInput"
-                    className='form-control'
-                    placeholder="Name"
-                    type="text"
-                    value={search}
-                    onChange={handleChange}
-                    onKeyPress={handleKeyPress}
-                  />
-                  <div>
-                    <div id="myBtn" className="ball-container pokeball-spin" onClick={handleClick} />
-                  </div>
+            <Col xs={12} md={5}>
+              <Row>
+                <div className="search-bar">
+                <input
+                  id="myInput"
+                  className='form-control'
+                  placeholder="Name"
+                  type="text"
+                  value={search}
+                  onChange={handleChange}
+                  onKeyPress={handleKeyPress}
+                />
+                <div>
+                  <div id="myBtn" className="ball-container pokeball-spin" onClick={handleClick} />
+                </div>
+              </div>
+            </Row>
+            <Row>
+              <Pokemon pokemon={pokemon} />
+            </Row>
+            </Col>
+            <Col xs={12} md={2}>
+              <div className="divisor-container">
+                <div className="divisor-line" />
+              </div>
+            </Col>
+            
+            {/* TODO: @media max-height: 400px;
+    padding-bottom: 1rem; */}
+            <Col xs={12} md={5} className="col-pokemon-information">
+              <Row>
+                <div className="container-information">
+                  {pokemon &&
+                    <>
+                    <div className="information">
+                      <label>Altura: </label><span>{pokemon?.height / 10} m</span>
+                    </div>
+
+                    <div className="information">
+                      <label>
+                        Peso: 
+                      </label>
+                      <span>{pokemon?.weight / 10} kg</span>
+                    </div>
+
+                    <div className="information">
+                      <label>
+                        Abilities: 
+                      </label>
+                    </div>
+                      <div className='abilities'>
+                        <ul>
+                        {pokemon?.abilities.map(ability =>
+                          <li className="ability-item" key={`ability-${ability.ability.name}`}>{ability.ability.name}</li>)}
+                        </ul>
+                      </div>
+                  </>}
                 </div>
               </Row>
               <Row>
-                <Pokemon name={name} />
+                <div className="d-flex flex-direction-row justify-content-between">
+                  <div className="pokemon-type">
+                    {pokemon?.types.map(type => `${type.type.name.charAt(0).toUpperCase()}${type.type.name.slice(1)}`)}
+                  </div>
+
+                  <div className="pokemon-type">
+                    {pokemon && `#${pokemon?.id}`}
+                  </div>
+                </div>
               </Row>
-              </Col>
-              <Col xs={12} md={2}>
-                <div className="divisor-container">
-                  <div className="divisor-line" />
-                </div>
-              </Col>
-              <Col xs={12} md={5}>
-              <div className='container-information'>
-                {pokemon &&
-                  <>
-                  <div className="information">
-                    <label>Altura: </label><span>{pokemon?.height / 10} m</span>
-                  </div>
-
-                  <div className="information">
-                    <label>
-                      Peso: 
-                    </label>
-                    <span>{pokemon?.weight / 10} kg</span>
-                  </div>
-
-                  <div className="information">
-                    <label>
-                      Abilities: 
-                    </label>
-                      <ul>
-                        {pokemon?.abilities.map(ability => <li>{ability.ability.name}</li>)}
-                      </ul>
-                  </div>
-                </>}
-                </div>
-              </Col>
+            </Col>
           </Row>
           <div className="lines">
             <div className="line-one" />
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import API from './api';
+import React from 'react';
 
-const Pokemon = ({ name }) => {
-  const [pokemon, setPokemon] = useState(null);
-
-  useEffect(() => {
-    if (name) {
-      API.get(`/pokemon/${name}`)
-        .then((res) => setPokemon(res.data))
-        .catch((err) => console.log(err))
-    }
-  }, [name]);
-
-  return (
-    <div className="pokemon-information">
-      <div className='pokemon-image'>
-        <div className='image'>
-          {pokemon &&
-            <img height="200px" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt="pokemon" />}
-          </div>
-      </div>
-      <div className='pokemon-name'>
-      {pokemon?.name.charAt(0).toUpperCase()}{pokemon?.name.slice(1)}
+const Pokemon = ({ pokemon }) => (
+  <div className="pokemon-information">
+    <div className='pokemon-image'>
+      <div className='image'>
+        {pokemon &&
+          <img height="200px" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt="pokemon" />}
       </div>
     </div>
-  )
-}
+    <div className='pokemon-name'>
+      {pokemon?.name.charAt(0).toUpperCase()}{pokemon?.name.slice(1)}
+    </div>
+  </div>
+);
 
 export default Pokemon;
